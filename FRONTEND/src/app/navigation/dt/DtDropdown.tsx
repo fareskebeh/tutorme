00f8ec1c96bbcf4facf70efcb2a6b94e9f8c87cc
fareskebeh <fares.kebbeh@gmail.com>
2025-06-23 16:40:07 -0700

    Finished desktop navigation

---
+++ FRONTEND/src/app/navigation/dt/DtDropdown.tsx
@@ -0,0 +1,51 @@
+import {useState, useRef, useEffect} from 'react'
+import { HiChevronDown } from 'react-icons/hi2'
+import {motion, AnimatePresence} from "framer-motion"
+import {Link} from "react-router-dom"
+
+type Props = {
+  dropContent: string[],
+  label:string,
+}
+
+const DtDropdown = ({label,dropContent}: Props) => {
+  const[open,setOpen] = useState<boolean>(false)
+  const menuRef = useRef<HTMLDivElement | null>(null)
+
+  useEffect(()=> {
+    const close = (e: MouseEvent) => {
+      const tg = menuRef.current;
+      if(tg && e.target instanceof Element && !tg.contains(e.target)) {
+        setOpen(false)
+      }
+      
+    }
+
+    document.addEventListener("click", close)
+
+      return()=> document.removeEventListener("click", close)
+  },[])
+
+  return (
+    <div ref={menuRef} onClick={()=>setOpen(!open)} className={`*:cursor-pointer p-2 rounded-lg active:bg-neutral-400/50 transition duration-300 relative items-center flex gap-2`}>
+      <p>{label==="tutors" ? "Find a Tutor" : "Find Tutor jobs" }</p>
+      <button className={`transition duration-300 ${open? "rotate-180" : ""}`}>
+        <HiChevronDown/>
+      </button>
+      
+      <AnimatePresence>
+      { open &&
+      <motion.div initial={{opacity:0, y:-20}} exit={{opacity:0, y:-20}} animate={{opacity:1, y:0}} transition={{duration:0.2}} className='flex flex-col gap-1 absolute z-60 p-2 w-full *:p-2 bg-white top-10 border rounded-xl border-neutral-400/70'>
+        {
+          dropContent.map((drop,index)=> (
+            <Link onClick={()=>setOpen(false)} className={`cursor-pointer rounded-lg ${location.pathname=== `/${label}/${drop.toLowerCase()}` ? "bg-emerald-500 text-white" : "hover:bg-neutral-200"}`} to={`${label==="tutors" ? "/tutors" : "/jobs"}/${drop.toLowerCase()}`} key={index}>{drop}</Link>
+          ))
+        }
+      </motion.div>
+      }
+      </AnimatePresence>
+    </div>
+  )
+}
+
+export default DtDropdown
--- FRONTEND/src/app/navigation/dt/DtNav.tsx
+++ FRONTEND/src/app/navigation/dt/DtNav.tsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import DtDropdown from "./DtDropdown";
+import tutorme from "../../../assets/tutorme.png"
+import { useContext } from "react";
+import { authContext } from "../../../state/authState";
+import blankPfp from "../../../assets/blank-profile-picture-hd-images-photo-1.jpeg"
 
-type Props = {}
-
-const DtNav = (props: Props) => {
+const DtNav = () => {
+  const auth = useContext(authContext)
+    if(!auth) {
+      throw new Error("authContext is undefined")
+    }
+    const {user} = auth;
+  const dropContent: string[] = ["All", "Online", "Home"];
+  const dropdowns: string[] = ["tutors", "jobs"];
   return (
-    <div>DtNav</div>
-  )
-}
+    <div className="py-3 px-4 flex items-center justify-between fixed top-0 left-0 right-0">
+    
+      <Link to="/home">
+        <img className="w-12" src={tutorme} alt="" />
+      </Link>
+      <div className="flex items-center gap-4">
+        {
+          dropdowns.map((drop,index)=> (
+            <DtDropdown key={index} label={drop} dropContent={dropContent}/>
+          ))
+        }
+        <Link className="transition duration-150 hover:scale-102 active:scale-100 p-2 bg-emerald-500  text-white rounded-lg" to="/login">
+          Request a tutor
+        </Link>
 
-export default DtNav
+        {
+          user? 
+          (
+            <Link className="contents" to="/dashboard">
+              <img className="w-10 rounded-full" src={blankPfp} alt="" />
+            </Link>
+          )
+          :
+          (
+            <Link className="p-2 hover:scale-102 active:scale-100 transition duration-150  border border-neutral-400 rounded-md" to="/login">Log In</Link>
+          )
+        }
+      </div>
+    </div>
+  );
+};
+
+export default DtNav;
