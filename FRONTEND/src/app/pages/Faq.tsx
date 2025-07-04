3a244252adf659bdae6909592ff9eeb4c8fb3449
fareskebeh <fares.kebbeh@gmail.com>
2025-07-04 14:12:49 -0700

    Created a component with conditional rendering

--- FRONTEND/src/app/pages/Faq.tsx
+++ FRONTEND/src/app/pages/Faq.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import {useState,useEffect} from "react"
+import MbFaq from "./MbFaq"
+import DtFaq from "./DtFaq"
 
-type Props = {}
+const Faq = () => {
 
-const Faq = (props: Props) => {
+  const[vp,setVp] = useState<string>("")
+  
+    useEffect(()=> {
+  
+        const adjVp=()=> {
+          setVp(window.innerWidth <800 ? "small" : "wide")
+        }  
+  
+        window.addEventListener("resize",adjVp);
+        
+        adjVp();  
+  
+        return ()=> window.removeEventListener("resize",adjVp)
+      },[])
+  
   return (
-    <div>Faq</div>
+    <div className="px-8 pb-8 pt-20 flex h-[100dvh] flex-col">
+      <div>
+        <p className="mb-4 text-4xl font-bold">Frequently Asked Questions</p>
+        <p className="text-lg mb-4 sm:text-xl">If you don’t see your question here, feel free to contact us at support@tutorme.com.</p>
+      </div>
+
+      {vp === "small" ? <MbFaq/> : <DtFaq/>}
+    </div>
   )
 }
 
