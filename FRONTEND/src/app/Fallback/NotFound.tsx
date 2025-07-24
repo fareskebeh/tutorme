807cabd37ea80024e9611c930aa5d295a8a19788
fareskebeh <fares.kebbeh@gmail.com>
2025-07-24 04:34:17 -0700

    feat: not found page styled

--- FRONTEND/src/app/Fallback/NotFound.tsx
+++ FRONTEND/src/app/Fallback/NotFound.tsx
@@ -1,8 +1,14 @@
-
+import NotfoundIcon from "../../assets/not-found.png"
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className="pt-20 flex h-[100dvh] px-8 items-center justify-center">
+      <div className="flex flex-col justify-center items-center gap-2 ">
+        <img src={NotfoundIcon}/>
+        <p className="text-2xl font-bold sm:text-3xl">Oops! This isn't the page you're looking for</p>
+        <p className="text-xl sm:text-2xl text-neutral-700">Try using different keywords!</p>
+      </div>
+    </div>
   )
 }
 
