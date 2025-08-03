6463ee446b884aec5e28b62e28ee3bb490739773
fareskebeh <fares.kebbeh@gmail.com>
2025-08-03 02:34:23 -0700

    chore: new files

---
+++ FRONTEND/src/app/tutors/TutorsLayout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const TutorsLayout = (props: Props) => {
+  return (
+    <div></div>
+  )
+}
+
+export default TutorsLayout
---
+++ FRONTEND/src/app/tutors/TutorsList.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const TutorsList = (props: Props) => {
+  return (
+    <div>TutorsList</div>
+  )
+}
+
+export default TutorsList
