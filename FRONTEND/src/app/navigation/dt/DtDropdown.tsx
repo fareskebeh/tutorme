eb56f1eda8fbd440373186270d877ec8ff581c7c
fareskebeh <fares.kebbeh@gmail.com>
2025-08-03 02:33:23 -0700

    fix: changed link structure to align with path

--- FRONTEND/src/app/navigation/dt/DtDropdown.tsx
+++ FRONTEND/src/app/navigation/dt/DtDropdown.tsx
@@ -34,10 +34,10 @@ const DtDropdown = ({label,dropContent}: Props) => {
       
       <AnimatePresence>
       { open &&
-      <motion.div initial={{opacity:0, y:-20}} exit={{opacity:0, y:-20}} animate={{opacity:1, y:0}} transition={{duration:0.2}} className='flex flex-col gap-1 absolute z-60 p-2 w-full *:p-2 bg-white top-10 border rounded-xl border-neutral-400/40'>
+      <motion.div initial={{opacity:0, y:-20}} exit={{opacity:0, y:-20}} animate={{opacity:1, y:0}} transition={{duration:0.2}} className='flex flex-col gap-1 absolute z-999 p-2 w-full *:p-2 bg-white top-10 border rounded-xl border-neutral-400/40'>
         {
           dropContent.map((drop,index)=> (
-            <Link onClick={()=>setOpen(false)} className={`cursor-pointer rounded-lg ${location.pathname=== `/${label}/${drop.toLowerCase()}` ? "bg-emerald-500 text-white" : "hover:bg-neutral-100 transition duration-200"}`} to={`${label==="tutors" ? "/tutors" : "/jobs"}/${drop.toLowerCase()}`} key={index}>{drop}</Link>
+            <Link onClick={()=>setOpen(false)} className={`cursor-pointer rounded-lg ${location.pathname=== `/${label}/filter/${drop.toLowerCase()}` ? "bg-emerald-500 text-white" : "hover:bg-neutral-100 transition duration-200"}`} to={`${label==="tutors" ? "/tutors/filter" : "/jobs"}/${drop.toLowerCase()}`} key={index}>{drop}</Link>
           ))
         }
       </motion.div>
