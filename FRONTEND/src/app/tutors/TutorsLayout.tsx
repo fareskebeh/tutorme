b6762b3637699bae8d6928b81f8504c3a165e8cf
fareskebeh <fares.kebbeh@gmail.com>
2025-08-23 17:37:38 +0300

    feat: hide search+filters when in the preview page

--- FRONTEND/src/app/tutors/TutorsLayout.tsx
+++ FRONTEND/src/app/tutors/TutorsLayout.tsx
@@ -4,6 +4,7 @@ import { Outlet } from "react-router-dom";
 import MbSearch from "./MbSearch";
 import { HiAdjustments, HiSearch } from "react-icons/hi";
 import { AnimatePresence } from "framer-motion";
+import { useLocation } from "react-router-dom";
 
 type Props = {
   vp: string;
@@ -11,14 +12,20 @@ type Props = {
 
 const TutorsLayout = (props: Props) => {
   const[sOpen,setSOpen] = useState<boolean>(false)
+  const {pathname} = useLocation()
 
   return (
     <div className="pt-20">
+      {
+
+      !pathname.includes("/tutors/id")  &&
+      <>
       <AnimatePresence>
         {sOpen && (
             <MbSearch setSOpen={setSOpen}/>
         )}
       </AnimatePresence>
+      
       {props.vp === "wide" ? (
         <div className="w-full fixed top-15 bg-white z-[899] shadow-md p-4 flex justify-between items-center">
           <Search/>
@@ -30,6 +37,9 @@ const TutorsLayout = (props: Props) => {
           <HiAdjustments size={24} />
         </div>
       )}
+      </>
+      }
+      
       <Outlet/>
     </div>
   );
