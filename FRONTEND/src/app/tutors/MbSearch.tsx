923fa493d9eb5f19dc51279d50afe374f16fb540
fareskebeh <fares.kebbeh@gmail.com>
2025-08-23 14:50:35 +0300

    fix: mobile search modal close button function

--- FRONTEND/src/app/tutors/MbSearch.tsx
+++ FRONTEND/src/app/tutors/MbSearch.tsx
@@ -43,51 +43,52 @@ const MbSearch = (props: Props) => {
         transition={{ duration: 0.15 }}
         className="p-4 bg-white h-[60%] flex flex-col justify-between shadow-md rounded-2xl w-[90%]"
       >
-        <div className="pb-2">
-          <HiX size={24} className="float-right"/>
+        <div className="pb-4 pr-2">
+          <HiX onClick={()=> props.setSOpen(false)} size={24} className="float-right"/>
         </div>
-          <div className="flex *:caret-emerald-400 flex-col gap-2 w-full">
-            <div className="relative w-full">
-              <input
-                value={query.subject}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                  setQuery({ ...query, subject: e.target.value })
-                }
-                className="bg-neutral-100 px-4 outline-none w-full p-2 rounded-xl text-xl"
-                placeholder="Enter Subject"
-                type="text"
-              />
 
-              {query.subject && (
-                <HiX
-                  onClick={()=> setQuery({...query, subject: ""})}
-                  className="absolute right-3 text-neutral-400 top-2.5"
-                  size={25}
-                />
-              )}
-            </div>
-            <div className="relative w-full">
-              <input
-                value={query.location}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                  setQuery({ ...query, location: e.target.value })
-                }
-                className="bg-neutral-100 px-4 outline-none w-full p-2 rounded-xl text-xl"
-                placeholder="Enter Location"
-                type="text"
-              />
+        <div className="flex *:caret-emerald-400 flex-col gap-2 w-full">
+          <div className="relative w-full">
+            <input
+              value={query.subject}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setQuery({ ...query, subject: e.target.value })
+              }
+              className="bg-neutral-100 px-4 outline-none w-full p-2 rounded-xl text-xl"
+              placeholder="Enter Subject"
+              type="text"
+            />
 
-              {query.location && (
-                <HiX
-                  onClick={()=> setQuery({...query, location: ""})}
-                  className="absolute right-3 text-neutral-400 top-2.5"
-                  size={25}
-                />
-              )}
-            </div>
+            {query.subject && (
+              <HiX
+                onClick={()=> setQuery({...query, subject: ""})}
+                className="absolute right-3 text-neutral-400 top-2.5"
+                size={25}
+              />
+            )}
           </div>
+          <div className="relative w-full">
+            <input
+              value={query.location}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setQuery({ ...query, location: e.target.value })
+              }
+              className="bg-neutral-100 px-4 outline-none w-full p-2 rounded-xl text-xl"
+              placeholder="Enter Location"
+              type="text"
+            />
 
-        <div className="flex flex-col text-xl text-center *:select-none gap-4 justify-center items-center">
+            {query.location && (
+              <HiX
+                onClick={()=> setQuery({...query, location: ""})}
+                className="absolute right-3 text-neutral-400 top-2.5"
+                size={25}
+              />
+            )}
+          </div>
+        </div>
+
+        <div className="flex flex-1 flex-col text-xl text-center *:select-none gap-4 justify-center items-center">
           <HiSearch size={50} />
           <p className="w-[70%]">Enter keywords to start searching</p>
         </div>
