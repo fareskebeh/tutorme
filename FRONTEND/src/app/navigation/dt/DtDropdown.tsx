168b60f2faa4e83acfb2e2ebde64a04fcc485744
fareskebeh <fares.kebbeh@gmail.com>
2025-08-23 14:50:11 +0300

    fix: stacking order issue fix

--- FRONTEND/src/app/navigation/dt/DtDropdown.tsx
+++ FRONTEND/src/app/navigation/dt/DtDropdown.tsx
@@ -26,7 +26,7 @@ const DtDropdown = ({label,dropContent}: Props) => {
   },[])
 
   return (
-    <div ref={menuRef} onClick={()=>setOpen(!open)} className={`*:cursor-pointer p-2 rounded-lg hover:bg-neutral-100/50 active:bg-neutral-200/50 transition duration-300 relative items-center flex gap-2`}>
+    <div ref={menuRef} onClick={()=>setOpen(!open)} className={`*:cursor-pointer p-2  rounded-lg hover:bg-neutral-100/50 active:bg-neutral-200/50 transition duration-300 relative items-center flex gap-2`}>
       <p>{label==="tutors" ? "Find a Tutor" : "Find Tutor jobs" }</p>
       <button className={`transition duration-300 ${open? "-rotate-180" : ""}`}>
         <HiChevronDown/>
@@ -34,7 +34,7 @@ const DtDropdown = ({label,dropContent}: Props) => {
       
       <AnimatePresence>
       { open &&
-      <motion.div initial={{opacity:0, y:-20}} exit={{opacity:0, y:-20}} animate={{opacity:1, y:0}} transition={{duration:0.2}} className='flex flex-col gap-1 absolute z-999 p-2 w-full *:p-2 bg-white top-10 border rounded-xl border-neutral-400/40'>
+      <motion.div initial={{opacity:0, y:-20}} exit={{opacity:0, y:-20}} animate={{opacity:1, y:0}} transition={{duration:0.2}} className='flex z-[999] flex-col gap-1 absolute p-2 w-full *:p-2 bg-white top-10 border rounded-xl border-neutral-400/40'>
         {
           dropContent.map((drop,index)=> (
             <Link onClick={()=>setOpen(false)} className={`cursor-pointer rounded-lg ${location.pathname=== `/${label}/filter/${drop.toLowerCase()}` ? "bg-emerald-500 text-white" : "hover:bg-neutral-100 transition duration-200"}`} to={`${label==="tutors" ? "/tutors/filter" : "/jobs"}/${drop.toLowerCase()}`} key={index}>{drop}</Link>
