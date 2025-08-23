6ae2953708bcf5114a30e08ec949659786bf6187
fareskebeh <fares.kebbeh@gmail.com>
2025-08-23 13:19:05 +0300

    feat: mock dataset for tutors

---
+++ FRONTEND/src/app/mock/types.ts
@@ -0,0 +1,42 @@
+export type Subject = {
+  title: string;
+  grade: string;
+};
+
+export interface User {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  username: string;
+  age: number;
+  location: string;
+  pfp?: string;
+  role: "Tutor" | "Admin" | "Client";
+  language: string;
+}
+
+export interface Client extends User {
+  parentContactInfo?: {
+    email: string;
+    phone: string;
+    name: string;
+    relation: string;
+  };
+}
+
+export interface Tutor extends User {
+  availability:
+    | "Busy"
+    | "Slightly available"
+    | "Available"
+    | "Highly available";
+  rate: number;
+  rating: number;
+  reviewsCount:number;
+  tags?: string[];
+  isFavorite?: boolean;
+  subjects: Subject[];
+}
+
+
