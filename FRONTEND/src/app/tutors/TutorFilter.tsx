d291b91c63d9ed95b7639e2e991e86cba6cbe552
fareskebeh <fares.kebbeh@gmail.com>
2025-08-23 14:26:40 +0300

    feat: Consumed the static dataset

--- FRONTEND/src/app/tutors/TutorFilter.tsx
+++ FRONTEND/src/app/tutors/TutorFilter.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import tutors from '../mock/tutors'
+import TutorTile from './TutorTile';
 
-type Props = {}
-
-const TutorFilter = (props: Props) => {
+const TutorFilter = () => {
   return (
-    <div>TutorFilter</div>
+    <div className='p-2 md:pt-18 flex bg-neutral-100 flex-col gap-2'>
+      {
+        tutors.map((tutor,i)=> (
+          <React.Fragment key={i}>
+          <TutorTile {...tutor} />
+          </React.Fragment>
+        ))
+        
+      }
+    </div>
   )
 }
 
