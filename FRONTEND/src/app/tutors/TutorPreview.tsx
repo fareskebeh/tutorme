a78ed4d6f9243d2c900f83960ce2993b6acaeb2e
fareskebeh <fares.kebbeh@gmail.com>
2025-08-24 14:16:39 +0300

    feat: routing to action routes if no user, created a multipurpose modal + passed user as props to review

--- FRONTEND/src/app/tutors/TutorPreview.tsx
+++ FRONTEND/src/app/tutors/TutorPreview.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useEffect, useContext, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import type { Tutor } from "../mock/types";
 import tutors from "../mock/tutors";
 import blankPfp from "../../assets/icons/pfp.png";
@@ -8,15 +8,25 @@ import {
   HiOutlineCurrencyDollar,
   HiOutlineHeart,
   HiOutlineLocationMarker,
-  HiOutlineChat
+  HiOutlineChat,
 } from "react-icons/hi";
 import Reviews from "./Reviews";
 import Rating from "../../components/Rating";
+import Modal from "../../components/Modal";
+import { AnimatePresence } from "framer-motion";
+import { authContext } from "../../state/authState";
 
 const TutorPreview = () => {
+  const navigate = useNavigate();
+
+  const auth = useContext(authContext);
+  if (!auth) {
+    throw new Error("authContext is undefined");
+  }
+  const { user } = auth;
   const { id } = useParams();
   const [tutor, setTutor] = useState<Tutor | undefined>(undefined);
-
+  const [modal, setModal] = useState<boolean>(false);
   useEffect(() => {
     const currTutor: Tutor | undefined = tutors.find(
       (tutor) => tutor.id === id
@@ -25,8 +35,35 @@ const TutorPreview = () => {
       setTutor(currTutor);
     }
   });
+
+  const performAction = (action: "book" | "chat" | "favorite") => {
+    if (user) {
+      if (action === "book") {
+        navigate(`/tutors/book/${id}`);
+      }
+      if (action === "chat") {
+        navigate(`/tutors/chat/${id}`);
+      }
+      if (action === "favorite") {
+        // logic to favorite/unfavorite, next feature probs
+      }
+    } else {
+      setModal(true);
+    }
+  };
+
   return (
     <div className="sm:pt-18 p-4 px-8 flex sm:flex-row flex-col gap-8">
+      <AnimatePresence>
+        {modal && (
+          <Modal
+            setModal={setModal}
+            confirm="Log In"
+            abort="Cancel"
+            prompt="To use this feature, you must be logged in"
+          />
+        )}
+      </AnimatePresence>
       <div className="flex flex-[0.6] flex-col items-start gap-4 sm:gap-6 md:gap-8">
         <div className=" flex items-center gap-4 ">
           <img
@@ -62,7 +99,7 @@ const TutorPreview = () => {
         </div>
         {tutor?.rating && (
           <div className="flex flex-col gap-1 sm:gap-2">
-            <Rating rating={tutor?.rating} reviewsCount={tutor?.reviewsCount}/>
+            <Rating rating={tutor?.rating} reviewsCount={tutor?.reviewsCount} />
             <div className="flex flex-col sm:gap-8 sm:flex-row sm:items-center text-neutral-500 items-start gap-1">
               <div className="flex items-center gap-1">
                 <HiOutlineClock /> {tutor?.availability}
@@ -76,15 +113,24 @@ const TutorPreview = () => {
           </div>
         )}
         <div className="flex *:flex *:items-center *:gap-2 gap-2 *:rounded-xl *:p-2 *:px-3 *:text-xl">
-          <button className="bg-emerald-500 cursor-pointer shadow-md hover:opacity-90 active:opacity-80 transition duration-150 text-white">
+          <button
+            onClick={() => performAction("book")}
+            className="bg-emerald-500 cursor-pointer shadow-md hover:opacity-90 active:opacity-80 transition duration-150 text-white"
+          >
             <HiOutlineClock /> Book
           </button>
-          <button className="bg-neutral-100 shadow-md text-neutral-500 cursor-pointer hover:opacity-90 active:opacity-80 transition duration-150">
+          <button
+            onClick={() => performAction("chat")}
+            className="bg-neutral-100 shadow-md text-neutral-500 cursor-pointer hover:opacity-90 active:opacity-80 transition duration-150"
+          >
             <HiOutlineChat /> Chat
           </button>
 
           {tutor?.isFavorite ? (
-            <button className="bg-emerald-100 shadow-md cursor-pointer hover:opacity-90 active:opacity-80 transition duration-150 text-emerald-500">
+            <button
+              onClick={() => performAction("favorite")}
+              className="bg-emerald-100 shadow-md cursor-pointer hover:opacity-90 active:opacity-80 transition duration-150 text-emerald-500"
+            >
               <HiOutlineHeart /> Unfavorite
             </button>
           ) : (
@@ -96,7 +142,11 @@ const TutorPreview = () => {
       </div>
 
       <div className="flex-[0.4]">
-        <Reviews reviews={tutor?.reviews} reviewCount={tutor?.reviewsCount} />
+        <Reviews
+          user={user}
+          reviews={tutor?.reviews}
+          reviewCount={tutor?.reviewsCount}
+        />
       </div>
     </div>
   );
