c157a0603e3e41ccc4622e694651396944031ee6
fareskebeh <fares.kebbeh@gmail.com>
2025-08-25 16:30:54 +0300

    feat: joined the filter trigger button to the filter component

--- FRONTEND/src/app/tutors/TutorsLayout.tsx
+++ FRONTEND/src/app/tutors/TutorsLayout.tsx
@@ -2,9 +2,10 @@ import { useState } from "react";
 import Search from "./Search";
 import { Outlet } from "react-router-dom";
 import MbSearch from "./MbSearch";
-import { HiAdjustments, HiSearch } from "react-icons/hi";
+import { HiSearch } from "react-icons/hi";
 import { AnimatePresence } from "framer-motion";
 import { useLocation } from "react-router-dom";
+import Filters from "../filters/Filters";
 
 type Props = {
   vp: string;
@@ -13,7 +14,6 @@ type Props = {
 const TutorsLayout = (props: Props) => {
   const[sOpen,setSOpen] = useState<boolean>(false)
   const {pathname} = useLocation()
-
   return (
     <div className="pt-20">
       {
@@ -27,14 +27,14 @@ const TutorsLayout = (props: Props) => {
       </AnimatePresence>
       
       {props.vp === "wide" ? (
-        <div className="w-full fixed top-15 bg-white border border-neutral-100 z-[899] shadow-md p-4 flex justify-between items-center">
+        <div className="w-full fixed top-15 bg-white border-b border-neutral-100 z-[350] *:z-[350] shadow-md p-4 flex justify-between items-center">
           <Search/>
-          <HiAdjustments size={24}/>
+          <Filters/>
         </div>
       ) : (
         <div className="shadow-md w-full flex p-4 justify-between items-center">
           <HiSearch className="active:bg-neutral-100 rounded-full" onClick={()=> setSOpen(true)} size={24} />
-          <HiAdjustments size={24} />
+          <Filters/>
         </div>
       )}
       </>
