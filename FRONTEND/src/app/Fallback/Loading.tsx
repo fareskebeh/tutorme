76977be864c26cc3e6bff254c0d0c2c4b0a92795
fareskebeh <fares.kebbeh@gmail.com>
2025-10-20 17:44:27 +0300

    Suspense loader styling

--- FRONTEND/src/app/Fallback/Loading.tsx
+++ FRONTEND/src/app/Fallback/Loading.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
-
-type Props = {}
-
-const Loading = (props: Props) => {
+const Loading = () => {
   return (
-    <div>Loading</div>
+    <div className="fixed top-0 left-0 right-0 bottom-0 z-[9999] flex justify-center items-center bg-black/40 dark:bg-slate-950/60">
+      <div className="loader-two"/>
+    </div>
   )
 }
 
