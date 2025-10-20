eea08a2a56febde789e2bd09f5691314472fd37d
fareskebeh <fares.kebbeh@gmail.com>
2025-10-20 16:48:56 +0300

    added transitions for the preview loader

--- FRONTEND/src/app/Fallback/loading/TutorPrevLoader.tsx
+++ FRONTEND/src/app/Fallback/loading/TutorPrevLoader.tsx
@@ -1,6 +1,6 @@
 const TutorPrevLoader = () => {
   return (
-    <div className="sm:pt-18 p-4 px-8 flex justify-center sm:flex-row flex-col w-full gap-8 animate-pulse">
+    <div className="sm:pt-18 p-4 px-8 flex **:transition duration-150 justify-center sm:flex-row flex-col w-full gap-8 animate-pulse">
       <div className="flex flex-[0.6] flex-col items-start gap-4 sm:gap-6 md:gap-8">
         <div className="flex items-center gap-4">
           <div className="h-30 w-30 sm:h-50 sm:w-50 rounded-full bg-neutral-300 dark:bg-slate-900" />
