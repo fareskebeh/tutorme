1125b83802c22489d419b1f09c67daae4285326a
fareskebeh <fares.kebbeh@gmail.com>
2025-10-20 10:49:01 +0300

    dark theme for Error state + Loading state on TutorsList

--- FRONTEND/src/app/Fallback/error/TutorListError.tsx
+++ FRONTEND/src/app/Fallback/error/TutorListError.tsx
@@ -2,7 +2,7 @@ import { HiExclamation } from 'react-icons/hi'
 
 const TutorListError = () => {
   return (
-    <div className='h-full flex flex-col rounded-xl shadow-md bg-white items-center justify-center'>
+    <div className='h-full flex flex-col rounded-xl shadow-md bg-white dark:bg-slate-950 transition duration-150 items-center justify-center'>
       <HiExclamation color='#ff0000' size={100}/>
         <p className='text-red-500 text-2xl w-80 text-center font-bold'>An error occurred, Try again later</p>
     </div>
--- FRONTEND/src/app/Fallback/loading/TutorsListLoader.tsx
+++ FRONTEND/src/app/Fallback/loading/TutorsListLoader.tsx
@@ -4,31 +4,31 @@ const TutorsListLoader = () => {
   return (
     Array.from({length:3}).map((_,i)=> (
       <div key={i}
-      className="flex p-4 justify-between h-80 bg-white shadow-md border border-neutral-100 rounded-xl"
+      className="flex p-4 justify-between h-80 bg-white dark:bg-slate-900 dark:border-slate-800 shadow-md border border-neutral-100 rounded-xl"
     >
       <div className="  justify-between flex flex-col gap-4 sm:gap-6 md:gap-8 ">
         <div className="flex gap-4 sm:gap-6 md:gap-8">
           <div
-            className="h-15 w-15 sm:h-30 bg-neutral-100 animate-pulse sm:w-30 rounded-full"
+            className="h-15 w-15 sm:h-30 bg-neutral-100 dark:bg-slate-800 animate-pulse sm:w-30 rounded-full"
           />
           <div className="flex *:flex *:items-center *:gap-1 flex-col justify-center gap-1">
-            <div className="h-12 w-50 bg-neutral-100 animate-pulse rounded-xl">
+            <div className="h-12 w-50 bg-neutral-100 dark:bg-slate-800 animate-pulse rounded-xl">
             </div>
-            <div className="h-8 w-30 bg-neutral-100 animate-pulse rounded-xl">
+            <div className="h-8 w-30 bg-neutral-100 dark:bg-slate-800 animate-pulse rounded-xl">
             </div>
-            <div className="h-8 w-20 bg-neutral-100 animate-pulse rounded-xl">
+            <div className="h-8 w-20 bg-neutral-100 dark:bg-slate-800 animate-pulse rounded-xl">
             </div>
           </div>
         </div>
 
-          <div className="flex flex-wrap *:p-2 *:animate-pulse *:h-7 *:w-10 *:bg-neutral-100 gap-2 *:rounded-xl">
+          <div className="flex flex-wrap *:p-2 *:animate-pulse *:h-7 *:w-10 *:dark:bg-slate-800 *:bg-neutral-100 gap-2 *:rounded-xl">
             {Array.from({length:3}).map((_, i) => (
               <div key={i}></div>
             ))}
           </div>
 
         <div className="flex flex-col gap-1 sm:gap-2">
-          <div className="flex flex-col sm:gap-8 sm:flex-row *:bg-neutral-100 *:animate-pulse *:rounded-xl sm:items-center items-start gap-1">
+          <div className="flex flex-col sm:gap-8 sm:flex-row *:bg-neutral-100 *:dark:bg-slate-800 *:animate-pulse *:rounded-xl sm:items-center items-start gap-1">
             <div className="flex items-center gap-1 h-10 w-30 ">
             </div>
             <div className="flex gap-2 *:h-10 *:w-10 *:p-2 *:rounded-xl ">
@@ -40,10 +40,10 @@ const TutorsListLoader = () => {
         </div>
       </div>  
       <div className="flex-col flex items-end justify-between">
-        <button className="w-10 rounded-full h-10 bg-neutral-100 animate-pulse ">
+        <button className="w-10 rounded-full h-10 bg-neutral-100 dark:bg-slate-800 animate-pulse ">
           
         </button>
-        <div className="w-20 rounded-xl h-10 bg-neutral-100 animate-pulse">
+        <div className="w-20 rounded-xl h-10 bg-neutral-100 dark:bg-slate-800 animate-pulse">
         </div>
       </div>
     </div>
